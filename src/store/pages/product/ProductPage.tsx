eddfce4a81dba351999648f04102e16ca2c92903
yuleidis Gutierrez <yuleidis.gtz@gmail.com>
2025-11-19 15:49:31 -0500

    feat(pages): Implementar y agregar ProductPage con Header y Jumbotron

--- src/store/pages/product/ProductPage.tsx
+++ src/store/pages/product/ProductPage.tsx
@@ -1,6 +1,12 @@
+import { CustomHeader } from "@/shared/components/CustomHeader"
+import { CustomJumbotron } from "@/store/components/CustomJumbotron"
 
 export const ProductPage = () => {
     return (
-        <div>ProductPage</div>
+        <>
+
+            <CustomHeader />
+            <CustomJumbotron description="Los mejores productos al mejor precio" />
+        </>
     )
 }

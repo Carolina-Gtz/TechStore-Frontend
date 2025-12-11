ab254238a5f978284595e5427d4c4fb74e96693b
yuleidis Gutierrez <yuleidis.gtz@gmail.com>
2025-12-11 11:17:31 -0500

    feat(app): configurar React Query, Devtools y cliente Axios
    - Se agregó QueryClientProvider y ReactQueryDevtools en el componente raíz
    - Se configuró la instancia de Axios usando VITE_API_URL
    - Se creó la interfaz ProductsResponse para tipar las respuestas de la API

--- src/TechStoreApp.tsx
+++ src/TechStoreApp.tsx
@@ -1,9 +1,16 @@
 import { RouterProvider } from "react-router"
 import { appRouter } from "./app.router"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
+const queryClient = new QueryClient();
 export const TechStoreApp = () => {
     return (
-        <RouterProvider router={appRouter} />
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={appRouter} />
+            {/* The rest of your application */}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
 
     )
 }

9a2e46c63fecf618cf41fee10bc8f4a207e564f7
yuleidis Gutierrez <yuleidis.gtz@gmail.com>
2026-01-17 09:03:41 -0500

    feat(auth-store): implementar acción checkAuthAction en Zustand para validar autenticación y refrescar JWT
    -Se implementa checkAuthAction para validar token y renovar, refrescar JWT y la data del usuario  o eliminarlo en el cache el token si ya expiro
    
    -implementar zustand en lo alto de la aplicacion para mantener el estado de  la data global  y el token  en cache dado por  checkAuthAction  o removerlo si expiro y/o refrescar el token cada vez que el usuario refreca la pagina o cada cierto tiempo

--- src/TechStoreApp.tsx
+++ src/TechStoreApp.tsx
@@ -1,13 +1,40 @@
 import { RouterProvider } from "react-router"
 import { appRouter } from "./app.router"
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query"
+import { checkAuthAction } from "./auth/actions/check-auth.action";
+import type { PropsWithChildren } from "react";
 
 const queryClient = new QueryClient();
+
+const CheckAuthProvider = ({ children }: PropsWithChildren) => {
+    const { data, isLoading } = useQuery({
+        queryKey: ['auth'],
+        queryFn: checkAuthAction,
+        retry: false,
+        staleTime: 1000 * 60 * 5,
+        refetchOnWindowFocus: true
+    });
+
+    if (isLoading) {
+        return <h1>Cargando sesión...</h1>;
+    }
+
+    console.log('Auth data:', data);
+
+    return children;
+}
+
 export const TechStoreApp = () => {
+
     return (
         <QueryClientProvider client={queryClient}>
-            <RouterProvider router={appRouter} />
+
+            {/**Custom Provider */}
+            <CheckAuthProvider>
+                <RouterProvider router={appRouter} />
+            </CheckAuthProvider>
+
             {/* The rest of your application */}
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
